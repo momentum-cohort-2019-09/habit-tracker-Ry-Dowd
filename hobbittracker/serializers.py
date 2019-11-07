256af97017ed38c09fc7ff12c521fa96c9f5d51e
Ryan <rydowd@gmail.com>
2019-11-07 13:20:37 -0500

    updated gitignore

---
+++ hobbittracker/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from hobbittracker.models import User, Habit, DailyLog, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Comment
+    fields = ['poster', 'habit', 'title', 'content', 'date']
+
+
+class DailyLogSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = DailyLog
+    fields = ['habit', 'actual', 'date', 'description']
+    
+    
+class HabitSerializer(serializers.ModelSerializer):
+  logs = DailyLogSerializer(many=True)
+  comments = CommentSerializer(many=True)
+  class Meta:
+    model = Habit
+    fields = ['user', 'observers', 'start_date', 'activity', 'goal', 'goal_measurement', 'comments', 'logs']
+  
+  
+class UserSerializer(serializers.ModelSerializer):
+  habits = HabitSerializer(many=True)
+  observed_habits = HabitSerializer(many=True)
+  comments = CommentSerializer(many=True)
+  class Meta:
+    model = User
+    fields = ['id', 'username', 'habits', 'observed_habits','comments',]
