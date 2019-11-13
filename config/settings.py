64a6bc7ada9571dd6ad482f1876c8ad7e4e637a2
Ryan <rydowd@gmail.com>
2019-11-13 15:48:31 -0500

    undo settings changes

--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,8 @@ ACCOUNT_ACTIVATON_DAYS = 7
 
 AUTH_USER_MODEL = 'hobbittracker.User'
 
+LOGIN_REDIRECT_URL = '/'
+
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
